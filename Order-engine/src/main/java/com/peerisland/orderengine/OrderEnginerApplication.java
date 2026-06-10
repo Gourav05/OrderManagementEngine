91dc01a1cc6120219f804d14500f54cd5d43a885
Gourav05 <gourav.chakraborty621@gmail.com>
2026-06-10 22:46:43 +0530

    First

---
+++ Order-engine/src/main/java/com/peerisland/orderengine/OrderEnginerApplication.java
@@ -0,0 +1,13 @@
+package com.peerisland.orderengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEnginerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderEnginerApplication.class, args);
+    }
+
+}
---
+++ Order-engine/src/test/java/com/peerisland/orderengine/OrderEnginerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peerisland.orderengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEnginerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
